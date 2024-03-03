9791fb783ce6c29cbf25c36caf98c45c2881d8e4
AkshayKate10 <akshaykate444@gmail.com>
2024-03-03 17:33:26 +0530

    cleanup main branch

--- src/main/java/com/temp/Main.java
+++ src/main/java/com/temp/Main.java
@@ -7,11 +7,6 @@ import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-
-        System.out.println(calculator.addition(4,3));
-        System.out.println(calculator.subtraction(4,5));
-        System.out.println(calculator.multiplication(4,3));
-        System.out.println(calculator.division(4,2));
+        System.out.println("hello");
     }
 }
--- src/test/java/com/temp/MainTest.java
+++
@@ -1,6 +0,0 @@
-package com.temp;
-
-import static org.junit.jupiter.api.Assertions.*;
-class MainTest {
-  
-}
