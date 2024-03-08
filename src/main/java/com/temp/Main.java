ac9a68d9d625dfc390a73569a048162c2475a996
AkshayKate10 <akshaykate444@gmail.com>
2024-03-08 11:35:05 +0530

    database

--- src/main/java/com/temp/Main.java
+++ src/main/java/com/temp/Main.java
@@ -6,7 +6,19 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class Main {
+    static Connection getDbConnection() {
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql", "root", "kawkaw12");
+            System.out.println("connection success");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("hello");
+        getDbConnection();
     }
 }
