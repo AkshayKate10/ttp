f73622ffddd3453b015af0879e352d0af4484e1b
AkshayKate10 <akshaykate444@gmail.com>
2024-03-25 17:15:16 +0530

    readme

--- src/main/java/com/temp/Main.java
+++ src/main/java/com/temp/Main.java
@@ -1,10 +1,5 @@
 package com.temp;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
